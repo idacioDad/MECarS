abbbc68f716e626661cadaeb3d0ace150583f793
Idácio Dad A. Zibia <43821712+idacioDad@users.noreply.github.com>
2020-07-20 13:00:19 +0200

    home Page

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,78 @@
+<!-- Header Section -->
+<header id="header" class="header-section container-fluid no-padding">
+			<!-- Container -->
+			<div class="container">
+				<!-- Top Header -->
+				<div class="top-header container-fluid no-padding">
+					<div class="col-md-8 col-sm-12 col-xs-12 top-icons no-padding">
+						<a href="#"><img src="images/chat-ic.png" alt="Chat" /><span>Click to</span><b>Chat</b></a>
+						<a href="#"><img src="images/phone-ic.png" alt="Phone" /><span>Call at</span><b>1-800-23-456-7890</b></a>
+						<ul>
+							<li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" title="Google+"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div><!-- Top Header /- -->		
+				<!-- nav -->
+				<nav class="navbar navbar-default ow-navigation">
+					<div class="navbar-header">
+						<button aria-controls="navbar" aria-expanded="false" data-target="#navbar" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a href="index.html" class="navbar-brand"><img src="images/logo.png" alt="Logo" /></a>
+					</div>
+					<!-- Menu Icon -->
+					<div class="menu-icon">
+						<div class="search">	
+							<a href="#" id="search" title="Search"><img src="images/search-ic.png" alt="Search" /></a>
+						</div>
+					</div><!-- Menu Icon /- -->
+					<div class="navbar-collapse collapse navbar-right" id="navbar">
+						<ul class="nav navbar-nav">
+							<li class="active dropdown">
+								<a href="index.html" title="Home" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
+								<i class="ddl-switch fa fa-angle-down"></i>
+								<ul class="dropdown-menu">				
+									<li><a href="index.html" title="homepage 1">homepage 1</a></li>
+									<li><a href="index2.html" title="homepage 2">homepage 2</a></li>
+								</ul>
+							</li>
+							<li><a href="about.html" title="About">About</a></li>
+							<li class="dropdown">
+								<a href="inventory.html" title="Inventory" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">inventory</a>
+								<i class="ddl-switch fa fa-angle-down"></i>
+								<ul class="dropdown-menu">				
+									<li><a href="inventory-single.html" title="Inventory Single">Inventory Single</a></li>
+								</ul>
+							</li>
+							<li><a href="services.html" title="Services">Services</a></li>
+							<li class="dropdown">
+								<a href="blog.html" title="Blog" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+								<i class="ddl-switch fa fa-angle-down"></i>
+								<ul class="dropdown-menu">				
+									<li><a href="blog-post.html" title="Blog Single">Blog Single</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false" title="Pages">Pages</a>
+								<i class="ddl-switch fa fa-angle-down"></i>
+								<ul class="dropdown-menu">				
+									<li><a href="404.html" title="404">404</a></li>
+								</ul>
+							</li>
+							<li><a href="contactus.html" title="Contact">Contact</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</nav><!-- nav /- -->
+				<!-- Search Box -->
+				<div class="search-box">
+					<span><i class="icon_close"></i></span>
+					<form><input type="text" class="form-control" placeholder="Enter a keyword and press enter..." /></form>
+				</div><!-- Search Box /- -->
+			</div><!-- Container /- -->
+		</header><!-- Header Section /- -->
+		
